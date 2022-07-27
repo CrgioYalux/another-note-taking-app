854e164156ee5a836a4d95fe9247d7facfd0c6d3
Sergio Yalux <randaledomman1@gmail.com>
2022-07-26 23:55:59 -0300

    feat(client): wrap app component with the notes database provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from './providers/ThemeProvider';
 import { ExecModeProvider } from './providers/ExecModeProvider';
+import { NoteDatabaseProvider } from './providers/NoteDatabaseProvider';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ExecModeProvider>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <NoteDatabaseProvider>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </NoteDatabaseProvider>
     </ExecModeProvider>
   </React.StrictMode>
 );
